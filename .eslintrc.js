99cd5c0c388e41a60ad172cdcbb5c3e5e4b31a1d
Tom Humphris <tom@muska.co.uk>
2016-10-27 12:12:46 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,48 @@ module.exports = {
   parserOptions: {
     sourceType: 'module'
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  extends: 'eslint:recommended',
   // required to lint *.vue files
   plugins: [
     'html'
   ],
   // add your custom rules here
   'rules': {
+    "indent": [
+        "error",
+        2
+    ],
+
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+
+    "quotes": [
+        "error",
+        "single"
+    ],
+
+    "semi": [
+        "error",
+        "always"
+    ],
+
+    "no-console": 0,
+
+    "no-undefined": 0,
+
     // allow paren-less arrow functions
     'arrow-parens': 0,
+
     // allow async-await
     'generator-star-spacing': 0,
+
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
