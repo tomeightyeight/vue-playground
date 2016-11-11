95826d4af1d2e908396efaf6ed3e4ff5fc9b369c
Tom Humphris <tom@muska.co.uk>
2016-11-11 14:38:20 +0000

    Replace vue-resource with native fetch

--- src/stores/Actions.js
+++ src/stores/Actions.js
@@ -1,35 +1,38 @@
 'use strict';
 
-import Vue from 'vue';
-import VueResource from 'vue-resource';
-
 import { REPLACE_TASKS } from './MutationTypes';
-
-Vue.use(VueResource);
 
 /**
  * Fetch tasks from end point
  */
 export const fetchTasks = function ({ commit }) {
-  Vue.http.get('/api/todos')
-    .then(response => {
-      commit({
-        type: REPLACE_TASKS,
-        tasks: response.body
-      });
-    }, error => {
-      console.log(error);
+  fetch('/api/todos', {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(response => {
+    commit({
+      type: REPLACE_TASKS,
+      tasks: response
     });
+  })
+  .catch(error => {
+    console.log(error);
+  });
 };
 
 /**
  * Persist tasks to server via end point
  */
 export const saveTasks = function ({ state }) {
-  Vue.http.put('/api/todo', state.tasks)
-    .then(() => {
-      alert('Task list saved!');
-    }, error => {
-      console.log(error);
-    });
+  fetch('/api/todo', {
+    method: 'PUT',
+    body: state.tasks
+  })
+  .then(() => {
+    console.log('task list saved');
+  })
+  .catch(error => {
+    console.log(error);
+  });
 };
